13f96816d7484bda2bbde5a980f82ac2f534183e
Mike Simons <simonsm2@gmail.com>
2013-05-16 14:58:24 -0400

    Added shared instance singleton method

--- Source/CrashlyticsLogger.h
+++ Source/CrashlyticsLogger.h
@@ -9,4 +9,6 @@
 
 @interface CrashlyticsLogger : DDAbstractLogger
 
++(CrashlyticsLogger*) sharedInstance;
+
 @end
--- Source/CrashlyticsLogger.m
+++ Source/CrashlyticsLogger.m
@@ -5,8 +5,19 @@
 //  Copyright (c) 2013 TechSmith. All rights reserved.
 //
 
+
+#define DEFINE_SHARED_INSTANCE_USING_BLOCK(block) \
+static dispatch_once_t pred = 0; \
+__strong static id _sharedObject = nil; \
+dispatch_once(&pred, ^{ \
+_sharedObject = block(); \
+}); \
+return _sharedObject; \
+
+
 #import "CrashlyticsLogger.h"
-#import <Crashlytics/Crashlytics.h>
+
+OBJC_EXTERN void CLSLog(NSString *format, ...) NS_FORMAT_FUNCTION(1,2);
 
 @implementation CrashlyticsLogger
 
@@ -26,5 +37,13 @@
 }
 
 
+
++(CrashlyticsLogger*) sharedInstance
+{
+   DEFINE_SHARED_INSTANCE_USING_BLOCK(^{
+   
+      return [[CrashlyticsLogger alloc] init];
+   });
+}
 
 @end
